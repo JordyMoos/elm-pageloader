eabaf9c6595ee79894c6fb5faca5a975d4bff3fc
Jordy Moos <jordymoos@gmail.com>
2017-12-14 23:00:40 +0100

    Add RemoteDataExt docs

--- src/PageLoader/DependencyStatus/RemoteDataExt.elm
+++ src/PageLoader/DependencyStatus/RemoteDataExt.elm
@@ -1,10 +1,24 @@
 module PageLoader.DependencyStatus.RemoteDataExt exposing (asStatus)
 
+{-| Convert a `RemoteData` to a `PageLoader.DependencyStatus.Status`
+
+This is only useful when you use the `RemoteData` library, but this will add `RemoteData` as a dependency.
+Therefor this should be moved to an additional optional dependency.
+
+
+# PageLoader.DependencyStatus.RemoteDataExt
+
+@docs asStatus
+
+-}
+
 import PageLoader.DependencyStatus as DependencyStatus
 import PageLoader.Progression as Progression
 import RemoteData
 
 
+{-| Converts a `RemoteData` to `PageLoader.DependencyStatus.Status`.
+-}
 asStatus : RemoteData.RemoteData e a -> DependencyStatus.Status
 asStatus remoteData =
     if RemoteData.isFailure remoteData then
