3d23a39b1bfc4e689d4d9cca33a1a40eeb351f49
Jordy Moos <jordymoos@gmail.com>
2017-12-15 00:29:24 +0100

    Add partial page loader data

--- src/PageLoader.elm
+++ src/PageLoader.elm
@@ -3,20 +3,44 @@ module PageLoader
         ( PageState(..)
         , TransitionStatus(..)
         , visualPage
+        , defaultTransitionStatusHandler
         , defaultDependencyStatusHandler
         , defaultDependencyStatusListHandler
-        , defaultTransitionStatusHandler
         )
+
+{-| `PageLoader` is a utility library to make loading of pages with dependencies clearer.
+
+
+# PageLoader
+
+@docs PageState, visualPage
+@docs TransitionStatus, defaultTransitionStatusHandler
+@docs defaultDependencyStatusHandler, defaultDependencyStatusListHandler
+
+-}
 
 import PageLoader.DependencyStatus as DependencyStatus
 import PageLoader.Progression as Progression
 
 
+{-| `PageState`
+A page can either be `Loaded` or `Transitioning`.
+
+`Loaded` has a `page` as payload.
+`Loaded` is used to display a page when all dependencies are loaded.
+
+`Transitioning` has a `page` (which usually is the previous page) and a `loader` (for the next page) as payload.
+The `Transitioning`s job is wait wait until the loader is been promoted to the new page.
+In the meantime the transition also holds a previous loaded page which can be used to display to the users.
+
+-}
 type PageState page loader
     = Loaded page
     | Transitioning page loader
 
 
+{-| `visualPage` gathers the `page` from a `PageState`.
+-}
 visualPage : PageState page loader -> page
 visualPage pageState =
     case pageState of
@@ -27,12 +51,19 @@ visualPage pageState =
             page
 
 
+{-| `TransitionStatus` is the result after an loader handles a msg.
+The `TransitionStatus` holds the loading data when it is `Pending`.
+It hold `data` (often the model of the new page) when the `TransitionStatus` is `Success`.
+And it holds an `String` representing an error message when the `TransitionStatus` is `Failed`.
+-}
 type TransitionStatus model msg data
     = Pending ( model, Cmd msg ) Progression.Progression
     | Success data
     | Failed String
 
 
+{-| Todo
+-}
 defaultDependencyStatusListHandler :
     ( model, Cmd msg )
     -> List DependencyStatus.Status
@@ -45,6 +76,8 @@ defaultDependencyStatusListHandler ( model, cmd ) dependencyStatuses onSuccessCa
         onSuccessCallback
 
 
+{-| Todo
+-}
 defaultDependencyStatusHandler :
     ( model, Cmd msg )
     -> DependencyStatus.Status
@@ -62,6 +95,8 @@ defaultDependencyStatusHandler ( model, cmd ) dependencyStatus onSuccessCallback
             Success (onSuccessCallback ())
 
 
+{-| Todo
+-}
 defaultTransitionStatusHandler :
     TransitionStatus loadingModel loadingMsg newData
     -> page
